93be3e17f6b0306ffec995571823670a5f9e64cd
j-piscitelli <jonahpiscitelli@gmail.com>
2025-08-26 09:57:06 -0400

    Update script_tutorial.R

--- code/script_tutorial.R
+++ code/script_tutorial.R
@@ -1,6 +1,6 @@
+# Example code ------------------------------------------------------------
 x <- c(1, 2)
 x
-
 y <- c(3, 4)
 y
 
@@ -15,3 +15,24 @@ sd(x)
 
 ## quick coding
 z <- c(1,2,3)
+
+
+# Exercises Aug 26 2025 ----------------------------------------------------------------
+
+#create a vector with 10 elements
+z<-c(1:10)
+zprime<-seq(1,10,length=10)
+zprimeprime<-letters[1:10]
+
+#create a matrix with 2 rows and 2 columns
+
+m<-matrix(1:4,nrow=2,ncol=2)
+m
+n<-cbind(c(1,2),c(3,4))
+n
+o<-rbind(c("a","b"),c("c","d"))
+o
+
+#data frame
+df0<-data.frame(name= c("Smith","John","Kate","Akira"),height=c(154, 170, 156, 175))
+df0
